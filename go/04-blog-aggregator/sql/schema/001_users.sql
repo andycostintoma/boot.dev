fe654399fe827e577ea9d26707c6096c74d4c5a1
Andy Toma <>
2025-01-13 20:30:02 +0200

    blog aggregator

--- go/04-blog-aggregator/sql/schema/001_users.sql
+++ go/04-blog-aggregator/sql/schema/001_users.sql
@@ -1,10 +1,11 @@
 -- +goose Up
-CREATE TABLE users(
-    id UUID PRIMARY KEY,
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name TEXT NOT NULL UNIQUE
+    name       TEXT      NOT NULL UNIQUE
 );
 
 -- +goose Down
-    DROP TABLE users;
+DROP TABLE users;
