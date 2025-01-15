52b9b892c77d90d5c3d2ed83379b79eabfe729d5
Andy Toma <>
2025-01-15 19:52:22 +0200

    blog aggregator

--- go/04-blog-aggregator/handler_agg.go
+++ go/04-blog-aggregator/handler_agg.go
@@ -6,10 +6,31 @@ import (
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <time_between_requests(1s, 1m, 1h, 1d)> ", cmd.Name)
+	}
+	return scrapeFeed(s, cmd)
+}
+
+func scrapeFeed(s *state, cmd command) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get next feed to fetch: %w", err)
+	}
+
+	feed, err := fetchFeed(context.Background(), nextFeed.Url)
 	if err != nil {
 		return fmt.Errorf("couldn't fetch feed: %w", err)
 	}
-	fmt.Printf("Feed: %+v\n", feed)
+
+	err = s.db.MarkFeedFetched(context.Background(), nextFeed.ID)
+	if err != nil {
+		return fmt.Errorf("couldn't mark feed as fetched: %w", err)
+	}
+
+	for _, item := range feed.Channel.Item {
+		fmt.Println(item.Title)
+	}
+
 	return nil
 }
