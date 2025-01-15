4437ea3ed32bbf1d6a90a8609afb6d0663a35be3
Andy Toma <>
2025-01-15 21:46:40 +0200

    blog aggregator

--- go/04-blog-aggregator/handler_agg.go
+++ go/04-blog-aggregator/handler_agg.go
@@ -9,10 +9,10 @@ func handlerAgg(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
 		return fmt.Errorf("usage: %s <time_between_requests(1s, 1m, 1h, 1d)> ", cmd.Name)
 	}
-	return scrapeFeed(s, cmd)
+	return scrapeFeed(s)
 }
 
-func scrapeFeed(s *state, cmd command) error {
+func scrapeFeed(s *state) error {
 	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
 	if err != nil {
 		return fmt.Errorf("couldn't get next feed to fetch: %w", err)
